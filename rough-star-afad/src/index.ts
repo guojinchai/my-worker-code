74ceadbdc98c8f3a4c96237101a9e056ff9532c9
guojinchai <godissake@gmail.com>
2024-01-09 16:22:15 +0800

    update formating

--- rough-star-afad/src/index.ts
+++ rough-star-afad/src/index.ts
@@ -27,63 +27,63 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const cookie = request.headers.get('Cookie');
 
-		  const cookie = request.headers.get("Cookie");
+		if (cookie && cookie.includes('CF_Authorization')) {
+			// Split the cookie string by semicolons and find the CF_Authorization value
+			const authValue = cookie
+				.split(';')
+				.find((c) => c.trim().startsWith('CF_Authorization'))
+				?.split('=')[1];
 
-		if (cookie && cookie.includes("CF_Authorization")) {
-		  // Split the cookie string by semicolons and find the CF_Authorization value
-		  const authValue = cookie
-			.split(";")
-			.find((c) => c.trim().startsWith("CF_Authorization"))
-			?.split("=")[1];
-
-		  if (authValue) {
-			// Send a request to the Cloudflare Access identity endpoint with the cookie
-			const identityUrl = `https://hackerwell.cloudflareaccess.com/cdn-cgi/access/get-identity`;
-			const identityResponse = await fetch(identityUrl, {
-			  headers: { Cookie: `CF_Authorization=${authValue}` },
-			});
-			// Parse the identity response as JSON
-			const identityData = await identityResponse.json() as any;
-
-			// Get the cf object from the request headers
-			const country = request.headers.get('CF-IPCountry');
-			const timeZone = request.headers.get('timezone') || "Asia/Kuala_Lumpur";
-			const timestamp = new Date().toLocaleString("en-US", {timeZone});
-			// Get the request URL
-			const url = new URL(request.url);
-			
-			// Check if the request path includes a country code
-			if (url.pathname === `/secure/${country}`){
-				// Get the flag asset from the R2 bucket using the country code as the key
-				const image = await env.MY_BUCKET.get(`${country}.png`)
-				
-				if (image === null) {
-					return new Response('Image Not Found', {status : 404});
-				}
-				// Return the flag asset as a response with the appropriate content type
-				return new Response(image.body,{
-					headers :{
-						"Content-Type": "image/png"
-					},
+			if (authValue) {
+				// Send a request to the Cloudflare Access identity endpoint with the cookie
+				const identityUrl = `https://hackerwell.cloudflareaccess.com/cdn-cgi/access/get-identity`;
+				const identityResponse = await fetch(identityUrl, {
+					headers: { Cookie: `CF_Authorization=${authValue}` },
 				});
-			}
 
-			// Return the email and timestamp from the identity data
-			//return new Response(`${identityData.email} authenticated at ${timestamp} from ${country}`);
-			const html = `<!DOCTYPE html>
-			<body>
-			  <p>${identityData.email} authenticated at ${timestamp} from <a href="${url.origin}/secure/${country}">${country}</a></p>
-			</body>`;
+				// Parse the identity response as JSON
+				const identityData = (await identityResponse.json()) as any;
+
+				// Get the cf object from the request headers
+				const country = request.headers.get('CF-IPCountry');
+				const timeZone = request.headers.get('timezone') || 'Asia/Kuala_Lumpur';
+				const timestamp = new Date().toLocaleString('en-US', { timeZone });
+				// Get the request URL
+				const url = new URL(request.url);
+
+				// Check if the request path includes a country code
+				if (url.pathname === `/secure/${country}`) {
+					// Get the flag asset from the R2 bucket using the country code as the key
+					const image = await env.MY_BUCKET.get(`${country}.png`);
+
+					if (image === null) {
+						return new Response('Image Not Found', { status: 404 });
+					}
+					// Return the flag asset as a response with the appropriate content type
+					return new Response(image.body, {
+						headers: {
+							'Content-Type': 'image/png',
+						},
+					});
+				}
+
+				// Return the email and timestamp from the identity data
+				//return new Response(`${identityData.email} authenticated at ${timestamp} from ${country}`);
+				const html = `<!DOCTYPE html>
+					<body>
+			  			<p>${identityData.email} authenticated at ${timestamp} from <a href="${url.origin}/secure/${country}">${country}</a></p>
+					</body>`;
 
 				return new Response(html, {
 					headers: {
-						"content-type": "text/html",
+						'content-type': 'text/html',
 					},
 				});
-		  }
+			}
 		}
-	 
-		 return new Response();
+
+		return new Response();
 	},
 };
